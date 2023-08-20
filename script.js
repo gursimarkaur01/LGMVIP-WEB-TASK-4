76b0c53466a5d70d5d799553dcf47f51079467ba
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-08-21 01:24:12 +0530

    refactor

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
